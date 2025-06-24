07cb4d5b802e135bcf79106568ca27570fe8a67c
karla <karla@10.54.238.116>
2025-06-24 15:12:26 -0600

    actualizacion

---
+++ src/GUI/VentanaFibonacci.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GUI;
+
+/**
+ *
+ * @author karla
+ */
+public class VentanaFibonacci extends VentanaOperaciones {
+    public VentanaFibonacci() {
+        super("SERIE DE FIBONACCI");
+        // Aquí va la interfaz...
+    }
+}
+
